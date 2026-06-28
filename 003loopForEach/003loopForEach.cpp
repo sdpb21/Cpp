a0c2610414dce09c7a02df3ec18243e79a3ddbef
sdpb21 <jytprf@gmail.com>
2026-06-28 19:14:16 -0300

    Loop for-each lesson completed on Sunday june 28, 2026 at 19:14

--- 003loopForEach/003loopForEach.cpp
+++ 003loopForEach/003loopForEach.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
 
-    
+    int ages [] {15,25,30,35,40};
+
+    for(int a: ages){
+
+        cout << a << endl;
+    }
+
+    double salaries [] {2500.56,1200.6,8650.3};
+
+    for(auto s: salaries){  // This is useful when you don't know the object or variable type
+
+        cout << s << endl;
+    }
+
+    string email = "john@informationpills.es";
+
+    for(auto e: email){
+
+        if( e == '@') cout << "@ found." << endl;
+        else cout << "@ not found." << endl;
+    }
+
+    vector<int> numbers;
+
+    int num;
+
+    cout << "Input some numbers. Exit with 0." << endl;
+
+    while (cin >> num && num != 0){
+
+        numbers.push_back(num);
+        
+    }
+
+    cout << "Numbers introduced:" << endl;
+
+    for( int n: numbers){
+
+        cout << n << endl;
+        
+    }
 }
