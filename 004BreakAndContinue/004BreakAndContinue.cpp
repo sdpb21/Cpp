e0faf9d09f7410724e175d387c85c96eb9605f61
sdpb21 <jytprf@gmail.com>
2026-06-29 19:50:16 -0300

    Working code Monday june 29, 2026 at 19:50

--- 004BreakAndContinue/004BreakAndContinue.cpp
+++ 004BreakAndContinue/004BreakAndContinue.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main(){
 
-    
+    vector<string> names={"Mary","Jane","Charles","Ana","Txulio","John","Joe"};
+
+    string random_name="Ana";
+
+    for (string n: names){
+
+        if ( (n.compare(random_name))==0 ){
+
+            cout << "Name found. Name: " << random_name << " found at position " << n << endl;
+
+            break;
+
+        }
+        
+    }
+
+    cout << "We've got out of loop." << endl;
+
 }
