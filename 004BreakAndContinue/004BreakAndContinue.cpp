30e0ace3a8236ded4cee3f84e7fda1b0214d6b5b
sdpb21 <jytprf@gmail.com>
2026-06-30 18:13:07 -0300

    Continue lesson completed on Tuesday June 30, 2026 at 18:13

--- 004BreakAndContinue/004BreakAndContinue.cpp
+++ 004BreakAndContinue/004BreakAndContinue.cpp
@@ -25,4 +25,25 @@ int main(){
 
     cout << "We've got out of loop." << endl;
 
+    vector<double> prices = {50, 100.0, 75, 150, 200, 300};
+
+    double discount = 0.15;
+
+    // Products with price under 100$ don't get discount
+    for (int i = 0; i < prices.size(); i++)
+    {
+        if ( prices[i]<100.0 )
+        {
+            continue;
+        }
+        
+        prices[i] -= prices[i]*discount;
+    }
+    
+    for (int i = 0; i < prices.size(); i++){
+
+        cout << "Final price from product " << (i+1) << ": " << prices[i] << "$" << endl;
+
+    }
+
 }
