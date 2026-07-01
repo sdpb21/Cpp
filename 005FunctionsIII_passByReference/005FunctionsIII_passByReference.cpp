e1618526186b5954e955678cfaa2724c23423d2a
sdpb21 <jytprf@gmail.com>
2026-07-01 18:07:37 -0300

    Pass to a function a value by reference lesson completed on Wednesday July 1st, 2026 at 18:07

--- 005FunctionsIII_passByReference/005FunctionsIII_passByReference.cpp
+++ 005FunctionsIII_passByReference/005FunctionsIII_passByReference.cpp
@@ -2,7 +2,38 @@
 #include <vector>
 using namespace std;
 
+void duplicateElements( vector<int> &vec){
+
+    for( int &val: vec){
+
+        val*=2;
+
+    }
+}
 int main(){
 
-    
+    vector<int> numbers = {1,2,3,4,5};
+
+    cout << "Original values: ";
+
+    for( int value: numbers){
+
+        cout << value << " ";
+
+    }
+
+    cout << endl;
+
+    duplicateElements(numbers);
+
+    cout << "Values after function call: ";
+
+    for( int value: numbers){
+
+        cout << value << " ";
+
+    }
+
+    cout << endl;
+
 }
